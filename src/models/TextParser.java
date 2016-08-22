566426352880026c99ae3d115a61cb407f5fa421
dylan-danse <dansedylan@gmail.com>
2016-08-23 01:55:54 +0200

    aaa

--- src/models/TextParser.java
+++ src/models/TextParser.java
@@ -8,7 +8,6 @@ package models;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.text.BreakIterator;
